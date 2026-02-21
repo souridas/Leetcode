35d2176dee8fff12bef741e5e75d11e6a78a2f9f
Souridas A <53730297+souridas@users.noreply.github.com>
2026-02-21 09:51:47 +0530

    Create 88-MergeSortedArray

---
+++ 88-MergeSortedArray/version/python/88-MergeSortedArray_20260221_095147.py
@@ -0,0 +1,27 @@
+# Last updated: 2/21/2026, 9:51:47 AM
+# O(m+n) start the pointer from the ends
+1class Solution:
+2    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+3        """
+4        Do not return anything, modify nums1 in-place instead.
+5        """
+6        i=m-1
+7        j=n-1
+8        k=m+n-1
+9        while i>=0 and j>=0:
+10            if nums1[i]>nums2[j]:
+11                nums1[k]=nums1[i]
+12                i-=1
+13                k-=1
+14            else:
+15                nums1[k]=nums2[j]
+16                j-=1
+17                k-=1
+18        while j>=0:
+19            nums1[k]=nums2[j]
+20            k-=1
+21            j-=1
+22        return
+23
+24
+25        
