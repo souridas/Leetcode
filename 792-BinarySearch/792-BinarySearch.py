20c16228a59c273e8643153c61a10fac4fe42c58
Souridas A <53730297+souridas@users.noreply.github.com>
2026-02-21 09:43:30 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.py
@@ -0,0 +1,16 @@
+# Last updated: 2/21/2026, 9:43:29 AM
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        i=0
+        j=len(nums)-1
+        while i<=j:
+            mid= (i+j)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                i=mid+1
+            else:
+                j=mid-1
+        return -1
+        
+        
