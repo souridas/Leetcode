c1f231230edaa3a5e83402c8e98aef5ff063aa42
Souridas A <53730297+souridas@users.noreply.github.com>
2026-02-21 09:44:51 +0530

    Create 72-EditDistance

---
+++ 72-EditDistance/72-EditDistance.cpp
@@ -0,0 +1,32 @@
+// Last updated: 2/21/2026, 9:44:51 AM
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n=word1.size();
+        int m=word2.size();
+        int dp[n+1][m+1];
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=m;j++)
+            {
+                if(i==0)
+                {
+                    dp[i][j]=j;
+                }
+                else if(j==0)
+                {
+                    dp[i][j]=i;
+                }
+                else if(word1[i-1]==word2[j-1])
+                {
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=1+min(min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
